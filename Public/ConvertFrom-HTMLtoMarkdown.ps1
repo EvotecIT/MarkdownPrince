e29129efe58ef9e1763217cb36215d43c98022f1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-07 10:32:58 +0200

    Update

--- Public/ConvertFrom-HTMLtoMarkdown.ps1
+++ Public/ConvertFrom-HTMLtoMarkdown.ps1
@@ -1,5 +1,18 @@
 ﻿function ConvertFrom-HTMLToMarkdown {
+    [cmdletBinding()]
     param(
-
+        [string] $Path,
+        [string] $DestinationPath,
+        [ReverseMarkdown.Config+UnknownTagsOption] $UnknownTags = [ReverseMarkdown.Config+UnknownTagsOption]::Drop,
+        [switch] $GithubFlavored
     )
+    if ($Path -and (Test-Path -Path $Path)) {
+        $Content = Get-Content -Path $Path
+
+        $Converter = [ReverseMarkdown.Converter]::new()
+        $Converter.Config.UnknownTags = $UnknownTags
+        $Converter.Config.GithubFlavored = $GithubFlavored.IsPresent
+        $ContentMD = $Converter.Convert($Content)
+        $ContentMD | Out-File -FilePath $DestinationPath
+    }
 }
