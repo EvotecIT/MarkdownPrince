97f70fc92be29411a47ef5531b86a305cb9de8a9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-08 16:14:47 +0200

    Update

---
+++ Examples/Example-01-ConvertFromHTML.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module $PSScriptRoot\..\MarkdownPrince.psd1 -Force -Verbose
+
+$HTMLFile = "$PSScriptRoot\Input\Example01.html"
+
+ConvertFrom-HTMLToMarkdown -Path $HTMLFile -UnknownTags Drop -GithubFlavored -DestinationPath $PSScriptRoot\Output\Example01.md
