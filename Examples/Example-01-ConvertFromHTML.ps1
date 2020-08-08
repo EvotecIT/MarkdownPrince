612a20a0721ec7672192985fd9b48298fd385fdb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-08 16:24:19 +0200

    Update examples

--- Examples/Example-01-ConvertFromHTML.ps1
+++ Examples/Example-01-ConvertFromHTML.ps1
@@ -1,5 +1,6 @@
-﻿Import-Module $PSScriptRoot\..\MarkdownPrince.psd1 -Force -Verbose
+﻿Import-Module $PSScriptRoot\..\MarkdownPrince.psd1 -Force
 
+# Define file to load
 $HTMLFile = "$PSScriptRoot\Input\Example01.html"
-
+# Convert it
 ConvertFrom-HTMLToMarkdown -Path $HTMLFile -UnknownTags Drop -GithubFlavored -DestinationPath $PSScriptRoot\Output\Example01.md
---
+++ Examples/Example-02-ConvertFromHTML.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module $PSScriptRoot\..\MarkdownPrince.psd1 -Force
+
+# Define file to load
+$HTMLFile = "$PSScriptRoot\Input\Example01.html"
+
+# Let me build small HTML to show it using PSWriteHTML
+# Install-Module PSWriteHTML
+New-HTML {
+    New-HTMLText -Text 'This is some text'
+    New-HTMLText -Text 'This is some other text'
+    New-HTMLTable -DataTable (Get-Process | Select-Object -First 5 -Property 'Id', 'Handles', 'CPU', 'SI', 'Name')
+    New-HTMLText -Text 'This is some other text continuing'
+    New-HTMLText -Text 'This is some other text'
+} -FilePath "$PSScriptRoot\Input\Example01.html"
+
+
+ConvertFrom-HTMLToMarkdown -Path $HTMLFile -UnknownTags Drop -GithubFlavored -DestinationPath $PSScriptRoot\Output\Example01.md
