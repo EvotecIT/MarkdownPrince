51b8e01e124b460fe1b0dad126f573a875f8d799
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-19 23:37:04 +0200

    Added text to remove code before and after processing

---
+++ Private/Remove-UnnessecaryContent.ps1
@@ -0,0 +1,11 @@
+﻿function Remove-UnnessecaryContent {
+    [cmdletBinding()]
+    param(
+        [string] $Content,
+        [Array] $Rules
+    )
+    foreach ($Rule in $Rules) {
+        $Content = $Content -replace $Rule
+    }
+    $Content
+}
