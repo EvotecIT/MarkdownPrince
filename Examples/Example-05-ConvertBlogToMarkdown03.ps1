3b24f7d71ed9fc713f8b9be1a9da661597b0e9e5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-28 16:39:39 +0200

    Code showing problem with tables #2

---
+++ Examples/Example-05-ConvertBlogToMarkdown03.ps1
@@ -0,0 +1,48 @@
+﻿Import-Module $PSScriptRoot\..\MarkdownPrince.psd1 -Force
+
+# Get Content
+$BlogContent = Invoke-WebRequest -Uri 'https://evotec.xyz/advanced-html-reporting-using-powershell/'
+$BlogContent.Content | Out-File -FilePath $PSScriptRoot\Input\Example05.html
+
+# Use PSParseHTML to format HTML so we can easier see what to remove
+#Format-HTML -File $PSScriptRoot\Input\Example04.html -OutputFile $PSScriptRoot\Input\Example04.html
+
+$RulesBefore = @(
+    '(?ms)<div id="logo" class="clearfix">(.*?)<\/div>'
+    '(?ms)<ul class="meta">(.*?)<\/ul>'
+    '(?ms)<title>(.*?)<\/title>'
+    '(?ms)<div class="left clearfix">(.*?)<\/div>'
+    '(?ms)<div class="right clearfix">(.*?)<\/div>'
+    '(?ms)<div id="icl_lang_sel_widget-3" class="widget sidebox widget_icl_lang_sel_widget">(.*?)<\/div>'
+    '(?ms)<div id="pageTitle" class="clearfix">(.*?)<\/div>'
+    '(?ms)<nav id="menu" class="cartfalse">(.*?)<\/nav>'
+    '(?ms)<nav id="breadcrumb" itemprop="breadcrumb">(.*?)<\/nav>'
+    '(?ms)<div class="wpml-ls-statics-footer wpml-ls wpml-ls-legacy-list-horizontal">(.*?)<\/div>'
+    '(?ms)<div id="oldie">(.*?)<\/div>'
+    '(?ms)<footer id="footer1" class="clearfix">(.*?)<\/footer>'
+    '(?ms)<footer id="footer2" class="clearfix">(.*?)<\/footer>'
+    '(?ms)<pre class="hidden">(.*?)</pre>'
+    # remove tags
+    '(?ms)<span class="meta-holder">(.*)<\/span>'
+    # we shouldn't have to play with script at all but seems to be a bug in reversemarkdown library
+    "(?ms)<script type='text/javascript' id='wp-util-js-extra'>(.*)<\/script>"
+    '(?ms)<script id="wp-util-js-extra">(.*)<\/script>'
+)
+$RulesAfter = @(
+    '<br>'
+)
+
+$Splat = @{
+    #Content                  = $BlogContent.Content
+    Path                     = "$PSScriptRoot\Input\Example05.html"
+    DestinationPath          = "$PSScriptRoot\Output\Example05.md"
+    UnknownTags              = 'Bypass'
+    GithubFlavored           = $true
+    RemoveComments           = $true
+    SmartHrefHandling        = $true
+    RulesBefore              = $RulesBefore
+    RulesAfter               = $RulesAfter
+    DefaultCodeBlockLanguage = 'powershell'
+    Format                   = $true
+}
+ConvertFrom-HTMLToMarkdown @Splat
